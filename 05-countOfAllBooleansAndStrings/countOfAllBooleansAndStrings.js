4e7921b340222d8e8e57fb58716e889125906de3
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 03:38:44 -0400

    Counts all booleans and strign using typeof

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,14 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  let count = 0;
+  // uses "typeof" to confirm is element at array is string or boolean
+  for(let i = 0, x = arr.length; i < x; i++){
+    if(typeof arr[i] == 'boolean' || typeof arr[i] == 'string'){
+      count ++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
 module.exports = countOfAllBooleansAndStrings;
+
