eafc81a61dd8bf10e9af1adc70c56a408be5ccc4
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 04:26:22 -0400

    Counts numbers within a range

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,6 +1,14 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+  let count = 0;
+  // For loop to iterate and add all numbers within parameters in array
+  for(let i = 0, x = nums.length; i < x; i++){
+    if(nums[i] <= end && nums[i] >= start){
+      count++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
 module.exports = sumOfNumsWithinARange;
+
