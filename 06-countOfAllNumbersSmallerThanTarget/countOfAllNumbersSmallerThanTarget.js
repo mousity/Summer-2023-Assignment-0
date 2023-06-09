cfa206c7491131b4b8138c88af0ba838518da232
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 03:44:50 -0400

    Counts up all numbers under target

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,12 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
-  // Insert code here;
+  let count = 0;
+  // Iterates through array and increments count if the number is below the target
+  for(let i = 0, x = nums.length; i < x; i++){
+    if(nums[i] < target){
+      count++;
+    }
+  }
+  return count++;
 }
 
 // Do not edit this line;
