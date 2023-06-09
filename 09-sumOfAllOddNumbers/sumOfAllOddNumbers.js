15d3bf2488153605329b54ad362839d537ed8326
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 04:24:16 -0400

    Similar concept to last assignment but confirming that numbers are odd before counting

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,6 +1,15 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+  let count = 0;
+  // For loop to iterate and add all odd numbers in array
+  for(let i = 0, x = nums.length; i < x; i++){
+    if((nums[i] % 2) == 1){
+      count++;
+    }
+  }
+  return count;
 }
+
 
 // Do not edit this line;
 module.exports = sumOfAllOddNumbers;
+
