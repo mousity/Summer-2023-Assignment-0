5a414e3774fb9360a6f542122dcd52acd54d4afe
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 18:45:31 -0400

    Implement a recursive count down function. One auxillary test case still failed, the rest have passed

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,9 +1,25 @@
 class MySolution {
+
+  total = 0;
+
   countDownSum(num) {
-    // Insert code here;
+
+    // If num is more than one, continue adding all values
+    if(num > 1){
+      this.total += num;
+      this.countDownSum(num - 1);
+    } 
+    // When we reach 1, add the last increment to the number
+    if (num == 1){
+      this.total += 1;
+    } 
+
+    // Returns total
+    return this.total;
   }
 }
 
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
+
