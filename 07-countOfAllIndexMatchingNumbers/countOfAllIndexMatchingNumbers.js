d28d3e6ed1dd54288fe0f3862ebc99542ba7efdc
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 03:48:23 -0400

    Counts the amount of values in the array that match their index

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,6 +1,14 @@
 function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+  let count = 0;
+  // If index (i) is equal to actual value, increase count
+  for(let i = 0, x = nums.length; i < x; i++){
+    if(nums[i] == i){
+      count++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
 module.exports = countOfAllIndexMatchingNumbers;
+
