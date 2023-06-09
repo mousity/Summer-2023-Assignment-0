1366d6e03e748cb25b057e714d6600587ac97f31
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 17:04:10 -0400

    Checks the front and back of a string to confirm if it is a palindrome

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,34 @@
 function isPalindrome(word) {
-  // Insert code here;
+
+  // Variable declarations to extract and compare letters from each side of the string
+  let letterOne, letterTwo;
+  let backStart = word.length - 1;  // Initializes the back of the string, where we will start
+  let comparisonCount = 0;          // We only need to iterate through half the array, so comparisonCount holds the amount of comparison we need to do
+
+  let isPalindrome = true;          // Boolean flag
+  
+  // Sets the comparisonCount variable
+  if(word.length % 2 == 0){
+    comparisonCount = word.length / 2;
+  } else {
+    comparisonCount = word.length / 2 - 0.5;
+  }
+
+  // Iterates through half the string, rounded down, and compares letters on both ends
+  for(let i = 0, x = comparisonCount; i < x; i++){
+    letterOne = word.charAt(i);
+    letterTwo = word.charAt(backStart);
+
+    if(letterOne != letterTwo){
+      isPalindrome = false;
+      break;
+    }
+    // Decrement to have the position on the back of the string match the front of the string
+    backStart--;
+  }
+
+  // Return the boolean flag
+  return isPalindrome;
 }
 
 // Do not edit this line;
