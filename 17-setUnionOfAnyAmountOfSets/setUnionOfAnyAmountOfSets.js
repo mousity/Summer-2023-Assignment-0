4d7994c2662e63472c2901005822b1f6034c44b0
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 19:34:21 -0400

    Adds all sets in a rest parameter to form one unique union set to return

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,6 +1,19 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  
+  // Create a temporary set to return set unions
+  const set = new Set();
+
+  // For each set in ...args
+  for(let i of args){
+    // For each element in the set
+    for(let j of i){
+      // Add the next element in the set
+      set.add(j);   // Properties of set objects are unique and we are in no danger of adding an element twice
+    }
+  }
+  return set;
 }
 
 // Do not edit this line;
 module.exports = setUnionOfAnyAmountOfSets;
+
