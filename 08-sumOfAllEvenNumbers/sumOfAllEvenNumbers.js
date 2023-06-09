a29c037b8585984df0364795014e09a0a18e18a4
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 04:21:05 -0400

    Counts the amount of even number in the array

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,6 +1,14 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+  let count = 0;
+  // For loop to iterate and add all even numbers in array
+  for(let i = 0, x = nums.length; i < x; i++){
+    if((nums[i] % 2) == 0){
+      count++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
 module.exports = sumOfAllEvenNumbers;
+
