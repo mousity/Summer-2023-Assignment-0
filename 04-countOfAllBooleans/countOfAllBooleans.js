79d3b082a2cd5c1581b1ffddb00b8b849f30fdfe
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 03:31:32 -0400

    Uses strict equality operator to confirm all booleans in an array

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,18 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  let count = 0;
+  // Iterates through array
+  for(let i = 0, x = arr.length; i < x; i++){
+    // The strict "===" operator must be used to confirm if an element is of type boolean
+    if(arr[i] === true){
+      count++;
+    }
+    if(arr[i] === false){
+      count++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
 module.exports = countOfAllBooleans;
+
