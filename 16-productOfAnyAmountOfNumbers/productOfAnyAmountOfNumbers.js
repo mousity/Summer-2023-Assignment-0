59f5367b5494184d422502972a0a6bdd510307ca
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 18:58:34 -0400

    Returns the product of all arguments in rest parameter

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,6 +1,13 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
+  // Total variable starts at 1 because multiplying by 0 would make every result 0
+  let total = 1;
+  // for each parameter in ...args, multiply and add to total
+  for (let i of args){
+    total *= i;
+  }
+  return total;
 }
 
 // Do not edit this line;
 module.exports = productOfAnyAmountOfNumbers;
+
