2ddafb522bfdd574e6a04ddd71f24a1d95b5329d
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 04:30:36 -0400

    Finds minimum and maximum values, returns total

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,16 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  // Variable declaration to give min and max base values
+  let min = nums[0];
+  let max = nums[0];
+  // Iteration loop, replacing min and max as it finds appropriate values
+  for(let i = 0, x = nums.length; i < x; i++){
+    if(nums[i] < min) {
+      min = nums[i];
+    } else if(nums[i] > max){
+      max = nums[i];
+    }
+  }
+  return min + max;
 }
 
 // Do not edit this line;
