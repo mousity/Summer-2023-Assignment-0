ba2e2f13191a6534ae4675d7a7051d7bea80484d
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 17:45:14 -0400

    Tracks frequency of each letter using an object with key-value pairs

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,25 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  
+  // Construct and epty object to store upcoming key-value pairs
+  let obj = {};
+
+  // For loop to iterate through the string
+  for(let i = 0, x = word.length; i < x; i++){
+
+    // If obj.key exists, simply add one to the existing value
+    if(obj[word[i]]){
+      obj[word[i]] += 1;
+
+    // If obj.ket does not exist, initialize it and give it the value of 1
+    } else {
+      obj[word[i]] = 1;
+    }
+  }
+
+  // Return object with finished key value pairs that reflect the frequency of letters
+  return obj;
 }
 
 // Do not edit this line;
 module.exports = frequencyCounter;
+
