ce5da040aa071663ecfc5bd8c41ea8bac9672984
Samr Mouna <samr.mouna@gmail.com>
2023-06-09 21:47:47 -0400

    Used binary search (without recursion, however) to find target value of array

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -4,10 +4,29 @@ class MySolution {
   }
 
   binarySearch(nums, target) {
-    // Insert code here;
+    let start = 0;
+    let end = nums.length;
+    let middle; 
+
+    while(start <= end){
+      middle = Math.floor((start + end) / 2);
+
+      if (nums[middle] == target){
+        this.flag = true;
+        break;
+      }
+
+      if (nums[middle] < target){
+        start = middle + 1;
+      } else {
+        end = middle - 1;
+      }
+    }
+    return this.flag;
   }
 }
 
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
+
